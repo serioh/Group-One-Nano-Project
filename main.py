3b16545380fc880348575ffcb3940d1eac2d9d35
becky <becky.zhao@outlook.com>
2021-12-20 17:33:15 +0000

    removed comment

--- main.py
+++ main.py
@@ -1,23 +1,4 @@
 """This is the main file"""
-
-"""
-Possible class ideas:
-class Question (create an instance of this class each time you want to show a question)
-- Will contain correct answer, wrong answer as an attribute
-- Attributes taken from connecting to the SQL server
-- Can use permutations itertool in a method to select how it will be displayed?
-
-class Player (create an instance of this class for each user)
-- Attributes (private ones probably) will include username, password, score history
-- Can login, delete player, show previous scores etc...
-
-class Test (create an instance every time you start a new test)
-- Brings up options to select difficulty of questions you want
-- Calculates total score from no. of questions you get right?
-- in main, create instance of test, then instances of questions
-
-
-"""
 import random
 from backend.question import Question
 from backend.snake_brain import SnakeBrain
